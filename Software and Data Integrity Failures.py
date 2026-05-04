ef243b7742ea4ecc09ed242ba5a6d62eafa37d08
Destiny016 <dharris481@ivytech.edu>
2026-05-04 17:32:25 -0500

    Add full OWASP code files

--- Software and Data Integrity Failures.py
+++ Software and Data Integrity Failures.py
@@ -1,13 +1,13 @@
 # Destiny Harris
 # 04/20/2026
-# 8 software and data integrity failures
+# 8. software and data integrity failures
 
 # Incorrect
 
-# <script src="https://cdn.example.com/lib.js"></script>
+<script src="https://cdn.example.com/lib.js"></script>
 
 # Fixed
 
-# <script src="https://cdn.example.com/lib.js"
-# integrity="sha384-abc123REPLACE_WITH_REAL_HASH"
-# crossorigin="anonymous"></script>
+<script src="https://cdn.example.com/lib.js"
+        integrity="sha384-abc123REPLACE_WITH_REAL_HASH"
+        crossorigin="anonymous"></script>
